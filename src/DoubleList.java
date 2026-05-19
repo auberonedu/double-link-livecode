592ea1dcf189e72b4cd6b4a1daf75251f3b55bed
Galatear <171226854+LadyGalatear@users.noreply.github.com>
2026-05-19 11:04:23 -0700

    Build DoubleList class

--- src/DoubleList.java
+++ src/DoubleList.java
@@ -1,10 +1,69 @@
 public class DoubleList {
-  public DoubleList() {
+  private class Node {
+    public int data;
+    public Node next;
+    public Node prev;
 
+    public Node(int data) {
+      this.data = data;
+    }
+  }
+
+  private Node head;
+  private Node tail;
+
+  public DoubleList() {
+    // head = null;
+    // tail = null;
+  }
+
+  // add to end
+  public void append(int data) {
+    Node newNode = new Node(data);
+
+    if (head == null) {
+      head = newNode;
+    } else {
+      tail.next = newNode;
+      newNode.prev = tail;
+    }
+
+    tail = newNode;
+  }
+
+  public int getFirst() {
+    if (head == null) {
+      throw new IndexOutOfBoundsException("Cannot get first data of empty list.");
+    }
+
+    return head.data;
+  }
+
+  // add to beginning
+  public void prepend(int data) {
+    Node newNode = new Node(data);
+
+    if (tail == null) {
+      tail = newNode;
+    } else {
+      head.prev = newNode;
+      newNode.next = head;
+    }
+
+    head = newNode;
+  }
+
+  public int getLast() {
+    if (tail == null) {
+      throw new IndexOutOfBoundsException("Cannot get last data of empty list.");
+    }
+
+    return tail.data;
   }
 
   @Override
   public String toString() {
+    // Print every node in the list, separated by spaces and commas
     return "To be implemented soon!";
   }
 }
